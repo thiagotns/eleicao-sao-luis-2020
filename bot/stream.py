bad93e9822346fcd4a27a8c1b69b82eb7a707d48
Thiago Nunes <thiagonunes.tns@gmail.com>
2020-09-22 14:31:56 -0300

    Handle 420 error

--- bot/stream.py
+++ bot/stream.py
@@ -9,19 +9,30 @@ from setup import create_api
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-
 class TweeterElectionListener(tweepy.StreamListener):
-    
+
     def __init__(self, output_file=sys.stdout):
         super(TweeterElectionListener, self).__init__()
         self.output_file = output_file
+        self.count = 0;
     
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
         print(tweet.text, file=self.output_file)
     
     def on_error(self, status_code):
-        logger.error(status_code)
+        logger.error(f"Error on Stream Listener. Code: {status_code}")
+        
+        if status_code == 420:
+            self.count = self.count + 1
+            delay = self.count * 120
+            logger.error(f"Waiting {delay} seconds")
+            time.sleep(delay)
+            return True
+
+        logger.error("Exiting the application")
+        return False
+
 
 def main():
 
