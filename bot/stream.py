5fd4632eb621f890da682d0fb3e78b9b67fb550e
Thiago Nunes <thiagonunes.tns@gmail.com>
2020-09-24 15:13:31 -0300

    Writing tweets to file

--- bot/stream.py
+++ bot/stream.py
@@ -1,6 +1,8 @@
 import sys
+import os
 import time
 import logging
+from datetime import datetime
 
 import tweepy
 
@@ -12,14 +14,17 @@ logger = logging.getLogger()
 
 class TweeterElectionListener(tweepy.StreamListener):
 
-    def __init__(self, output_file=sys.stdout):
+    def __init__(self, output_file):
         super(TweeterElectionListener, self).__init__()
         self.output_file = output_file
-        self.count = 0;
+        self.count = 0
     
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
-        print(tweet.text, file=self.output_file)
+        logger.info(tweet.text)
+        print(tweet._json, file=self.output_file)
+        self.output_file.flush()
+        os.fsync(self.output_file.fileno())
     
     def on_error(self, status_code):
         logger.error(f"Error on Stream Listener. Code: {status_code}")
@@ -40,13 +45,16 @@ def main():
     users_id = get_cadidates_twitter_user_id()
 
     api = create_api()
-    listener = TweeterElectionListener()
+    
+    output = open("data/stream_output.txt", "a")
+
+    listener = TweeterElectionListener(output_file=output)
     
     stream = tweepy.Stream(auth=api.auth, listener=listener)
 
     try:
         
-        logger.info('Start Streaming.')
+        logger.info(f'Start Streaming - {datetime.now()}.')
         stream.filter(follow=users_id)
 
     except KeyboardInterrupt as e :
@@ -54,6 +62,7 @@ def main():
     finally:
         logger.info('Stream Finished.')
         stream.disconnect()
+        output.close()
 
     return
 
